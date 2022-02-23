fe45e32f7541f4675ce111636e2e9b3f3aa02df2
James Hager <james@avantiapplications.com>
2022-02-23 14:50:38 -0700

    Update img src after submit

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ const p = document.querySelector('p')
 
 function handleSubmit(evt) {
 	evt.preventDefault();
-	// console.log('form submit');
-	form.style.display='none'
-    p.style.display='block'
-    document.querySelector('img').src="https://cataas.com/cat"
+	form.style.display = 'none'
+    p.style.display = 'block'
+    document.querySelector('img').src= "https://cataas.com/cat/says/Thanks"
 }
 
 form.addEventListener('submit', handleSubmit);
-p.style.display='none'
+p.style.display = 'none'
