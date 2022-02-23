eba28fa3ed52c1b3ba45c144fdd634c1c3e8ce04
James Hager <james@avantiapplications.com>
2022-02-23 15:11:16 -0700

    Fix index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function handleSubmit(evt) {
 	evt.preventDefault();
     const name = document.querySelector('input[name="name"]').value
 	form.style.display = 'none'
-    p.text = `Thanks for signing up ${name}!`
+    p.textContent = `Thanks for signing up ${name}!`
     p.style.display = 'block'
     document.querySelector('img').src= `https://cataas.com/cat/says/Thanks ${name}!`
 }
